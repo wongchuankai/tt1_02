e6c78730e2be8d99fd07fac006f226711d5af99d
CK <wongchuankai97@gmail.com>
2021-09-30 14:39:44 +0800

    server

--- server/db/db.js
+++ server/db/db.js
@@ -1,17 +1,13 @@
 const Pool = require('pg').Pool
-
-const db_name = "d3qbcvbetr1kmh";
-const db_password = "dc79e08abf80b691de9378647cd6f20feff9ded1f3d1de2d7ad166340f5153db";
-const db_user =   "tdhubcthrfnnrc";
-const db_host = "ec2-52-207-47-210.compute-1.amazonaws.com"
-
-const dev_config = {
-    user: db_user,
-    host: db_host,
-    database: db_name,
-    password: db_password,
-    port: 5432,
+const test_config = {
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    port: 5432
 }
+
+
 const proConfig = {
     connectionString: process.env.DATABASE_URL,
     ssl: {
@@ -20,7 +16,7 @@ const proConfig = {
 }
 
 const pool = new Pool(
-    process.env.NODE_ENV === "production" ? proConfig : proConfig
+    process.env.NODE_ENV === "production" ? proConfig : test_config
 )
 
 module.exports = pool
