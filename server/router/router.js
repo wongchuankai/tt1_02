147f9b12c87e056f01a66cdc93a12ebd743da163
CK <wongchuankai97@gmail.com>
2021-09-30 11:22:31 +0800

    git ignore nodemodules

--- server/router/router.js
+++ server/router/router.js
@@ -1,8 +1,11 @@
 const {Router} = require('express')
 const router = Router()
 
-const controller = require('../controller/controller')
+const user_controller = require('../controller/user-controller')
+const shop_controller = require('../controller/shop-controller')
 
-router.post("/signup", controller.signup )
+router.post("/signup", user_controller.signup )
+
+
 
 module.exports = router
