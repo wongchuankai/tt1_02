f42735a31d359616a36436eeb58498446b7fb7af
CK <wongchuankai97@gmail.com>
2021-09-30 11:30:10 +0800

    cors

--- server/queries/queries.js
+++ server/queries/queries.js
@@ -1,5 +1,7 @@
 const signup = "INSERT INTO Customer(username, password, first_name, last_name, postal_code, gender, created_at) VALUES($1,$2,$3,$4,$5,$6,$7)"
+const getUser = "Select * from users where username = $1"
 
 module.exports = {
-    signup
+    signup,
+    getUser
 }
