076d016e4dad9a6ae190ca12ce5b4b7e3c73b45e
Gabriel Tan <gabrieltan1996@gmail.com>
2021-09-30 11:09:38 +0800

    products display skeleton

---
+++ client/src/components/Products.js
@@ -0,0 +1,27 @@
+import React from "react";
+import products from "../Dataset/products.json";
+import Product from "./Product";
+
+const Products = () => {
+  return (
+    <main>
+      <h2>Products</h2>
+      <div>
+        {products.map((product) => {
+          const { id, title, price, description, image } = product;
+          return (
+            <Product
+              key={id}
+              title={title}
+              price={price}
+              desc={description}
+              img={image}
+            ></Product>
+          );
+        })}
+      </div>
+    </main>
+  );
+};
+
+export default Products;
