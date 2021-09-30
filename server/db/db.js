33ecf3409667d29fc717858881b1d899443adbf6
CK <wongchuankai97@gmail.com>
2021-09-30 14:26:48 +0800

    server

--- server/db/db.js
+++ server/db/db.js
@@ -1,11 +1,15 @@
 const Pool = require('pg').Pool
-require("dotenv").config()
+
+const db_name = process.env.POSTGRES_DB || "d3qbcvbetr1kmh";
+const db_password = process.env.POSTGRES_PASSWORD || "dc79e08abf80b691de9378647cd6f20feff9ded1f3d1de2d7ad166340f5153db";
+const db_user =  process.env.POSTGRES_USER || "tdhubcthrfnnrc";
+const db_host = "ec2-52-207-47-210.compute-1.amazonaws.com"
 
 const dev_config = {
-    user: process.env.LOCAL_POSTGRES_USER,
-    host: process.env.LOCAL_POSTGRES_HOST,
-    database: process.env.LOCAL_POSTGRES_DB,
-    password: process.env.LOCAL_POSTGRES_PASSWORD,
+    user: db_user,
+    host: db_host,
+    database: db_name,
+    password: db_password,
     port: 5432,
 }
 const proConfig = {
